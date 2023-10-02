80f489ba5b72a807e471ac6192f797a15f9b7a28
jaiinterviewmail <jaiinterviewmail@gmail.com>
2023-10-02 18:45:10 +0530

    remvoed all code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("added from git hub");
-        System.out.println("from java code chagnes");
-
-        System.out.println("second added git hub");
+        System.out.println("removed");
     }
 }
